d2543be8cea91d5be33db45b7116470e3cc3d2fb
dekunukem <dekunukem@gmail.com>
2022-02-11 21:42:03 +0000

    working on pc app

--- src/usb4vc_config_main.py
+++ src/usb4vc_config_main.py
@@ -8,6 +8,7 @@ from tkinter import filedialog
 from tkinter import simpledialog
 from tkinter import messagebox
 import urllib.request
+from appdirs import *
 
 THIS_VERSION_NUMBER = '0.0.1'
 MAIN_WINDOW_WIDTH = 800
@@ -18,6 +19,19 @@ HEIGHT_CONNECT_LF = 60
 default_button_color = 'SystemButtonFace'
 if 'linux' in sys.platform:
     default_button_color = 'grey'
+
+def ensure_dir(dir_path):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+appname = 'usb4vc_config'
+appauthor = 'dekuNukem'
+save_path = user_data_dir(appname, appauthor, roaming=True)
+backup_path = os.path.join(save_path, 'profile_backups')
+temp_path = os.path.join(save_path, 'temp_files')
+ensure_dir(save_path)
+ensure_dir(backup_path)
+ensure_dir(temp_path)
 
 root = Tk()
 root.title("USB4VC Configurator v" + THIS_VERSION_NUMBER)
@@ -33,6 +47,14 @@ def open_user_manual_url():
 def open_discord_link():
     webbrowser.open('https://discord.gg/HAuuh3pAmB')
 
+def backup_button_click():
+    if 'darwin' in sys.platform:
+        subprocess.Popen(["open", backup_path])
+    elif 'linux' in sys.platform:
+        subprocess.Popen(["xdg-open", backup_path])
+    else:
+        webbrowser.open(backup_path)
+
 INVALID_ROOT_FOLDER_STRING = "<-- Press & Select Your Flash Drive"
 dp_root_folder_display = StringVar()
 dp_root_folder_display.set(INVALID_ROOT_FOLDER_STRING)
@@ -41,9 +63,12 @@ root_folder_path_label = Label(master=connection_lf, textvariable=dp_root_folder
 root_folder_path_label.place(x=100, y=6)
 
 user_manual_button = Button(connection_lf, text="User Manual", command=open_user_manual_url)
-user_manual_button.place(x=570, y=5, width=100)
+user_manual_button.place(x=500, y=5, width=100)
 
 discord_button = Button(connection_lf, text="Discord", command=open_discord_link)
+discord_button.place(x=600, y=5, width=80)
+
+discord_button = Button(connection_lf, text="Backups", command=backup_button_click)
 discord_button.place(x=680, y=5, width=80)
 
 dp_root_folder_path = ''
@@ -55,6 +80,7 @@ def select_root_folder(root_path=None):
         return
     dp_root_folder_path = root_path
     dp_root_folder_display.set("Selected: " + root_path)
+    copy_button.config(state=NORMAL)
 
 open_button = Button(connection_lf, text="Open...", command=select_root_folder)
 open_button.place(x=10, y=5, width=80)
@@ -67,12 +93,14 @@ pc_app_update_label.place(x=10, y=40)
 pc_app_update_label.config(text='This app (' + str(THIS_VERSION_NUMBER) + '): Unknown', fg='black', bg=default_button_color)
 pc_app_update_label.unbind("<Button-1>")
 
-copy_button = Button(updates_lf, text="Copy latest updates to flash drive", command=None)
-copy_button.place(x=10, y=10, width=200)
+copy_button = Button(updates_lf, text="Copy Latest USB4VC Updates to Flash Drive", command=None, state=DISABLED)
+copy_button.place(x=10, y=10, width=300)
 
-mappings_lf = LabelFrame(root, text="Custom Mappings", width=MAIN_WINDOW_WIDTH - PADDING*2, height=420)
+mappings_lf = LabelFrame(root, text="Custom Gamepad Mappings", width=MAIN_WINDOW_WIDTH - PADDING*2, height=420)
 mappings_lf.place(x=PADDING, y=170)
 
-root.update()
+select_root_folder('C:/Users/allen/Desktop/flashdrive_test')
 
+
+root.update()
 root.mainloop()
